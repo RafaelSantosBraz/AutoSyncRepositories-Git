eb2ee60f8e4fc728ee33fa81274a4233d40d45c9
Rafael dos Santos Braz <rafinha.santos.braz@gmail.com>
2019-05-15 20:07:09 -0300

    Remoção de antigas classes e adição de pacote JSON

--- GitSync/GitSync/Program.cs
+++ GitSync/GitSync/Program.cs
@@ -7,24 +7,7 @@ namespace GitSync
     {
         static void Main(string[] args)
         {
-            if (args.Length != 1)
-            {
-                Console.WriteLine("Esperava encontrar um único parâmetro!");
-                return;
-            }           
-            Diretorio diretorio = new Diretorio(args[0]);
-            if (!diretorio.Verificar())
-            {
-                Console.WriteLine("Caminho informado não encontrado!");
-                return;
-            }
-            Configuracao config = new Configuracao("config.txt");
-            if (!config.Verificar())
-            {
-                Console.WriteLine("Erro ao manipular Usuário e Senha no arquivo de Configuração!");
-            }
-            GitControle controle = new GitControle(config, diretorio);
-            controle.CriarCommit("Teste");
+
         }
     }
 }
